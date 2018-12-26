48107cc8d3174875713c608deb1a94dfef853b01
serjmax <serj.max@gmail.com>
2018-12-26 16:08:00 +0200

    raname List comp

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,11 +1,14 @@
 import React, { PureComponent } from "react";
-import Tasks from "../containers/Tasks";
+
+import "./style.css";
+
+import Tasks from "../../containers/Tasks";
 
 class List extends PureComponent {
   render() {
     return (
-      <div className="List">
-        <div className="title">{this.props.list.name}</div>
+      <div className="list">
+        <div className="list__title">{this.props.list.name}</div>
         <Tasks />
       </div>
     );
