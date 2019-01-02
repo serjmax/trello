517ccbb6eaaf3b4e11ac1c4e2c0a353a03da56cb
serjmax <serj.max@gmail.com>
2019-01-02 13:19:06 +0200

    update styles for nav-bar

--- src/containers/NavBar/index.js
+++ src/containers/NavBar/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
+import "./style.css";
+
 class NavBar extends Component {
   render() {
     return (
-      <div>
+      <div className="nav-bar">
         <Link to="/">Все доски</Link>
       </div>
     );
