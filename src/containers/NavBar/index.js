81ba161d0c593b7f23aea96ae7a9dfd63b4d560b
serjmax <serj.max@gmail.com>
2019-01-12 15:18:37 +0200

    add board list to navbar

--- src/containers/NavBar/index.js
+++ src/containers/NavBar/index.js
@@ -1,13 +1,30 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
+import BoardService from "../../Services/BoardService";
+
 import "./style.css";
 
 class NavBar extends Component {
   render() {
     return (
       <div className="nav-bar">
-        <Link to="/">Все доски</Link>
+        <Link className="nav-bar__list-title" to="/">
+          Мои доски
+        </Link>
+        <ul className="nav-bar__links-list">
+          {BoardService.getBoards().map(board => (
+            <li className="nav-bar__links-item" key={board.id}>
+              <Link
+                className="nav-bar__links-link"
+                to={`/boards/${board.name}`}
+                board={board}
+              >
+                {board.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
