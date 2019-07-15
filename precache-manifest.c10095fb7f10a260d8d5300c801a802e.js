6d49ac34947bfccad9abcfa812e8652b300e8872
Sergey Maximov <Serj.max@gmail.com>
2019-07-15 08:28:22 +0200

    Updates

---
+++ precache-manifest.c10095fb7f10a260d8d5300c801a802e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "c753dc64c993055f2672",
+    "url": "/trello/static/js/runtime~main.c753dc64.js"
+  },
+  {
+    "revision": "f9edbad823e557d03a6c",
+    "url": "/trello/static/js/main.f9edbad8.chunk.js"
+  },
+  {
+    "revision": "a6909f9bc698f6fd4504",
+    "url": "/trello/static/js/1.a6909f9b.chunk.js"
+  },
+  {
+    "revision": "c46184b30de139d65dc6ff8076b3c12d",
+    "url": "/trello/index.html"
+  }
+];
